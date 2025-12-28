8aa5155bdf7874c398e68c4e9c221f207d499afd
official-notfishvr <notfishvrvr+6545@gmail.com>
2025-12-28 00:00:35 -0600

    moved stuff + testing for mimicAPI

---
+++ MimicAPI.TestMod/Loader.cs
@@ -0,0 +1,26 @@
+using MelonLoader;
+using MimicAPI.TestMod;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(Loader), "MimicAPI.TestMod", "1.0.0", "MimicAPI")]
+[assembly: MelonGame("ReLUGames", "MIMESIS")]
+
+namespace MimicAPI.TestMod
+{
+    public class Loader : MelonMod
+    {
+        private GameObject gui;
+
+        public override void OnInitializeMelon() { }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (gui == null)
+            {
+                gui = new GameObject(nameof(TestGUI));
+                gui.AddComponent<TestGUI>();
+                GameObject.DontDestroyOnLoad(gui);
+            }
+        }
+    }
+}
